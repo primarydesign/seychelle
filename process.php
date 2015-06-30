a8a653a83d988e92d5a9f13151fa168a15b927ee
Mitchell Gates <md.fragala@gmail.com>
2015-06-30 15:51:28 -0400

    Added fuller functionality and HTML template.

--- process.php
+++ process.php
@@ -1,31 +1,74 @@
 <?php
 
-//FORM INPUTS
-$fname = $_POST['first_name'];
-$lname = $_POST['last_name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$messg = $_POST['message'];
-
-//REGULAR EXPRESSIONS
-$regex = [
-	'email' => '/[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+([.][a-zA-Z]+)+$/',
-	'phone' => '/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/'
-];
-
-$to = "mitchell@primarydesign.com";
-$subject = "User Message from Seychell Penthouse";
-$from = "From:" . $email;
-
-$message = $fname . " " . $lname;
-$message .= "\n" . $email;
-$message .= "\n" . $phone;
-$message .= "\n" . $messg;
-
-if (mail($to, $subject, $message, $from)) {
-	echo "success";
-} else {
-	echo "failure";
+if (!isset($_POST['submit'])) {
+/* no submission data */
+	
+	include 'index.html.php';
+	
+} else { //Process Submission	
+/* submission data detected */
+	
+	//Load Submission Input
+	$fname = $_POST['first_name'];
+	$lname = $_POST['last_name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$messg = $_POST['message'];
+	
+	$clearance = 0;
+	
+	//Validate Requireds
+	$reqs = [$fname,$lname,$email,$phone];
+	foreach( $reqs as $r ) {
+		$clearance += (required($r)) ? 0 : 1;}
+	//Validate Email
+	$clearance += (email($email)) ? 0 : 1;
+	//Validate Phone
+	$clearance += (phone($phone)) ? 0 : 1;
+	
+	//Check Validation
+	if ($clearance !== 0) {
+	/* submission invalidation */
+		
+		include 'index.html.php';
+		
+	} else {
+	/* submission clear */	
+		
+		//Load mailer Data
+		$to = "mitchell@primarydesign.com";
+		$subject = "User Message from Seychell Penthouse";
+		$from = "From:" . $email;
+		$message = $fname . " " . $lname;
+		$message .= "\n" . $email;
+		$message .= "\n" . $phone;
+		$message .= "\n" . $messg;
+		
+		//Initialize Mailer
+		
+		
+		if (mail($to, $subject, $message, $from)) {
+			echo "success";
+		} else {
+			echo "failure";
+		}
+		
+	}
+}
+//GENERAL GLOBAL DECLARATIONS
+function required($x) {
+	if ($x !== "") return true;
+	else return false;
+}
+function email($x) {
+	$rgx = '/[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+([.][a-zA-Z]+)+$/';
+	$val = preg_match($rgx,$x);
+	return ($val === 1) ? true : false;
+}
+function phone($x) {
+	$rgx = '/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/';
+	$val = preg_match($rgx,$x);
+	return ($val === 1) ? true : false;
 }
 
 ?>
